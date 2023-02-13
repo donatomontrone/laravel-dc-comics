d67eadb73662412e81b1b65bcc873348f8fab9b2
donatomontrone <donato.montrone@outlook.com>
2023-02-13 15:59:43 +0100

    added home route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/comics', [ComicsController::class, 'index'])->name('comics.index');
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 
+Route::get('/comics', [ComicsController::class, 'index'])->name('comics.index');
 Route::get('/comics/{id}', [ComicsController::class, 'show'])->name('comics.show');
