40ff8768d36a8420a1d86119380697c78bc08b8d
donatomontrone <donato.montrone@outlook.com>
2023-02-14 15:43:17 +0100

    edit header

--- resources/views/partials/guests/header.blade.php
+++ resources/views/partials/guests/header.blade.php
@@ -26,7 +26,7 @@
             <div class="collapse navbar-collapse nav-list" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link fw-bold" href="#">HOME</a>
+                        <a class="nav-link fw-bold" href="{{route('guests.home')}}">HOME</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link fw-bold " aria-current="page" href="#">COMICS</a>
